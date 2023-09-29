9feee62839f36c08f297b3b582d21456b0b12b5a
altaparro <matiasaltaparro@hotmail.com>
2023-09-28 23:10:30 -0300

    modificado

--- coder2.js
+++ coder2.js
@@ -1,21 +1,28 @@
 // prueba de extensiones
 
+let entrada = Number(prompt("Seleccione la opción deseada: 1) Realizar suma 2) Realizar resta 3) Realizar multiplicación 4) Realizar división 5) Salir"));
 
-let entrada = Number(prompt("Seleccione la opción deseada: 1) Realizar suma 2) Realizar resta 3) Realizar multiplicación 4) Realizar división 5) Salir"))
-
-
-while (entrada != 5){
-    num1 = Number(prompt("ingrese el primer numero"));
-    num2 = Number(prompt("ingrese el segundo numero"));
-    switch(entrada){
-        case 1:
-         resultado= num1+num2;
-
-         alert("El resultado de la suma es: "+ resultado);
-         break;
-        
-
+    while (entrada != 5 && entrada > 0 && entrada <= 5) {
+        num1 = Number(prompt("ingrese el primer numero"));
+        num2 = Number(prompt("ingrese el segundo numero"));
+        switch (entrada) {
+            case 1:
+            alert("El resultado de la suma es: " + (num1 + num2));
+            break;
+            case 2:
+            alert("El resultado de la resta es: " + (num1 - num2));
+            break;
+            case 3:
+            alert("El resultado de la multiplicacion es: " + (num1 * num2));
+            break;
+            case 4:
+            alert("El resultado de la division es: " + (num1 / num2));
+            break;
+            default:
+            alert("Opción no válida.");
+        }
+        entrada = prompt("Seleccione la opción deseada: 1) Realizar suma 2) Realizar resta 3) Realizar multiplicación 4) Realizar división 5) Salir");
     }
-    entrada = prompt("Seleccione la opción deseada: 1) Realizar suma 2) Realizar resta 3) Realizar multiplicación 4) Realizar división 5) Salir")
-}
-prompt("Salio correctamente del sistema.")
+    
+    
+    alert("Salio correctamente del sistema.");
