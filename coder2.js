de39c92d38c2090c03b7f9ea04a148b2558df00e
altaparro <matiasaltaparro@hotmail.com>
2023-10-18 10:13:10 -0300

    while

--- coder2.js
+++ coder2.js
@@ -1,15 +1,9 @@
-//actividad 1
-
 function calcular() {
-
-    // Obtener los valores de los campos de entrada
-    
     var numero1 = parseFloat(document.getElementById('numero1').value);
     var numero2 = parseFloat(document.getElementById('numero2').value);
     var operador = document.getElementById('operador').value;
     var resultado = 0;
 
-    // Realizar el cálculo según el operador seleccionado
     if (operador === 'suma') {
         resultado = numero1 + numero2;
     } else if (operador === 'resta') {
@@ -25,6 +19,13 @@ function calcular() {
         }
     }
 
-    // Mostrar el resultado en la página
     document.getElementById('resultado').textContent = resultado;
+
+    setTimeout(function() {
+        if (confirm("¿Desea realizar otro cálculo?")) {
+            document.getElementById('numero1').value = '';
+            document.getElementById('numero2').value = '';
+            document.getElementById('resultado').textContent = '-';
+        }
+    }, 1);
 }
